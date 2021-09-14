c8a9e909bd730afe13318b6cfe2a8980e783819d
drserendipity <sschoi@postech.ac.kr>
2021-09-14 17:06:24 +0900

    First Commit

--- solutions/Computer Vision/2_Image Captioning/model.py
+++ solutions/Computer Vision/2_Image Captioning/model.py
@@ -9,7 +9,7 @@ class EncoderCNN(nn.Module):
         resnet = models.resnet50(pretrained=True)
         for param in resnet.parameters():
             param.requires_grad_(False)
-        
+
         modules = list(resnet.children())[:-1]
         self.resnet = nn.Sequential(*modules)
         self.embed = nn.Linear(resnet.fc.in_features, embed_size)
@@ -19,15 +19,38 @@ class EncoderCNN(nn.Module):
         features = features.view(features.size(0), -1)
         features = self.embed(features)
         return features
-    
+
 
 class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
-        pass
-    
+        super(DecoderRNN, self).__init__()
+        self.embed_size = embed_size
+        self.hidden_size = hidden_size
+        self.vocab_size = vocab_size
+        self.num_layers = num_layers
+        self.word_embeddings = nn.Embedding(vocab_size, embed_size)
+        self.lstm = nn.LSTM(input_size=embed_size, hidden_size=hidden_size, num_layers=num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, vocab_size)
+
     def forward(self, features, captions):
-        pass
+        embeds = self.word_embeddings(captions[:, :-1])
+        embeddings = torch.cat((features.unsqueeze(1), embeds), 1)
+        lstm_out, self.hidden = self.lstm(embeddings)
+        features = self.fc(lstm_out)
+        return features
 
     def sample(self, inputs, states=None, max_len=20):
         " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
-        pass
+        hidden = (torch.randn(self.num_layers, 1, self.hidden_size).to(inputs.device),
+                  torch.randn(self.num_layers, 1, self.hidden_size).to(inputs.device))
+
+        list_predictions = []
+        for i in range(max_len):
+            lstm_out, hidden = self.lstm(inputs, hidden)
+            out = self.fc(lstm_out).squeeze(1)
+            out_argmax = out.argmax(dim=1)
+            list_predictions.append(out_argmax.item())
+            inputs = self.word_embeddings(out_argmax).unsqueeze(1)
+
+        return list_predictions
+
